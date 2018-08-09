580911e726cc2eba9a598b8e3be85a4a596dab8f
armistec <32623927+armistec@users.noreply.github.com>
2018-08-08 17:00:54 -0700

    Add files via upload

---
+++ Map/tribes.js
@@ -0,0 +1,12 @@
+var headquarters =
+{
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "STATEFP": "41", "PLACENS": "02409531", "GEOID": "4178600", "NAME": "Warm Springs", "Pop": 5000, "ORIG_FID": 0 }, "geometry": { "type": "Point", "coordinates": [ -121.289842912939704, 44.766816516381759 ] } },
+{ "type": "Feature", "properties": { "STATEFP": "41", "PLACENS": "02411900", "GEOID": "4167500", "NAME": "Siletz", "Pop": 4804, "ORIG_FID": 1 }, "geometry": { "type": "Point", "coordinates": [ -123.918677065652631, 44.721834875918191 ] } },
+{ "type": "Feature", "properties": { "STATEFP": "41", "PLACENS": "02408316", "GEOID": "4130300", "NAME": "Grand Ronde", "Pop": 5200, "ORIG_FID": 2 }, "geometry": { "type": "Point", "coordinates": [ -123.62104014258486, 45.073849627407753 ] } },
+{ "type": "Feature", "properties": { "STATEFP": "53", "PLACENS": "02408915", "GEOID": "5348550", "NAME": "Nespelem Community", "Pop": 9500, "ORIG_FID": 3 }, "geometry": { "type": "Point", "coordinates": [ -119.024389371651594, 48.171491960923653 ] } },
+{ "type": "Feature", "properties": { "STATEFP": "32", "PLACENS": "02410428", "GEOID": "3222500", "NAME": "Elko", "Pop": 2090, "ORIG_FID": 4 }, "geometry": { "type": "Point", "coordinates": [ -115.767457237885594, 40.838658475122557 ] } },
+{ "type": "Feature", "properties": { "STATEFP": "16", "PLACENS": "02411624", "GEOID": "1645370", "NAME": "Lapwai", "Pop": 3500, "ORIG_FID": 5 }, "geometry": { "type": "Point", "coordinates": [ -116.804098303544066, 46.403338058428915 ] } }
+]
+}
